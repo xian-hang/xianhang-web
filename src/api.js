fb40adc20fe294121a0cb96c3f309667e316c32e
Alvintan0712 <tjan2000@gmail.com>
2022-05-11 14:38:24 +0800

    add cors headers

--- src/api.js
+++ src/api.js
@@ -8,6 +8,9 @@ const options = {
     headers: {
         "content-type": "application/x-www-form-urlencoded", 
         "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Credentials": "true", 
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT", 
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers", 
     }
 }
 
