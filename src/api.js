413fcbe28d6d6ef313a302c5e96b1bb4d075cbd5
Alvintan0712 <tjan2000@gmail.com>
2022-05-11 14:41:06 +0800

    add authorization

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ const options = {
         "Access-Control-Allow-Origin": "*", 
         "Access-Control-Allow-Credentials": "true", 
         "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT", 
-        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers", 
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization", 
     }
 }
 
